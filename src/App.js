b280ae60dae827efc7fe70fdbc030541e13714ce
Paul Borawski <paulborawski14@gmail.com>
2018-03-07 10:02:05 -0800

    creating extra files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-/*  return (
+   return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
-      ); */
-      return React.createElement('div', { className: "App" }, React.createElement('h1', { className: "Appchild"}, 'Does this work now?'))
+      );
+      //return React.createElement('div', { className: "App" }, React.createElement('h1', { className: "Appchild"}, 'Does this work now?'))
   }
 }
 
