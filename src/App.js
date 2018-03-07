c1859fff222290e65fdc06b3b6848e3996513633
Paul Borawski <paulborawski14@gmail.com>
2018-03-07 14:31:02 -0800

    using state to update names

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Paul', age: 30 },
+      { name: 'Max', age: 29 },
+      { name: 'Emily', age: 26 }
+    ]
+  }
+
   render() {
    return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <Person name="Paul" age="30"/>
-        <Person name="Max" age="29"/>
-        <Person name="Emily" age="26"/>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
       );
       //return React.createElement('div', { className: "App" }, React.createElement('h1', { className: "Appchild"}, 'Does this work now?'))
