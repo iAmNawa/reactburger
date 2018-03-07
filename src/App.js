b75ef083ff375f85fad0ff75429edb84e113ae93
Paul Borawski <paulborawski14@gmail.com>
2018-03-07 14:58:30 -0800

    using setState to change parts of state...STATE only works in classes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,15 @@ class App extends Component {
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked');
+    // console.log('Was clicked');
+    // DO NOT DO THIS this.state.persons[0].name = "Pauly";
+    this.setState({
+      persons: [
+      { name: 'Pauly', age: 30 },
+      { name: 'Max', age: 29 },
+      { name: 'Emily', age: 29 }
+    ]
+    })
   }
 
   render() {
@@ -21,9 +29,16 @@ class App extends Component {
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
         <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}></Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
       );
       //return React.createElement('div', { className: "App" }, React.createElement('h1', { className: "Appchild"}, 'Does this work now?'))
