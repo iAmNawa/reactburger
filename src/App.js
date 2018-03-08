225ca37347fd3d572b6bc61121c5932704c7c051
Paul Borawski <paulborawski14@gmail.com>
2018-03-07 18:06:06 -0800

    using ternary operator to display or not siplay on click.  If statements will not work here, you can only use ternary

--- src/App.js
+++ src/App.js
@@ -1,59 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Paul', age: 30 },
-      { name: 'Max', age: 29 },
-      { name: 'Emily', age: 26 }
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
-    // console.log('Was clicked');
-    // DO NOT DO THIS this.state.persons[0].name = "Pauly";
-    this.setState({
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
       persons: [
-      { name: newName, age: 30 },
-      { name: 'Max', age: 29 },
-      { name: 'Emily', age: 29 }
-    ]
-    })
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
   }
 
   nameChangedHandler = (event) => {
-    this.setState({
+    this.setState( {
       persons: [
-      { name: 'Paulr', age: 30 },
-      { name: event.target.value, age: 29 },
-      { name: 'Emilyr', age: 29 }
-    ]
-    })
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
   }
 
-  render() {
-   return (
+  togglePersonsHandler = () => {
+   const doesShow = this.state.showPersons;
+   this.setState({showPersons: !doesShow});
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
       <div className="App">
-        <h1>Hi, I'm a React app</h1>
+        <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('PaulyOne')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'PaulyTwo')}
-          changed={this.nameChangedHandler} >Hobbies: coding</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+        {  this.state.showPersons ?
+             <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Max!')}
+                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age} />
+            </div> : null
+       }
       </div>
-      );
-      //return React.createElement('div', { className: "App" }, React.createElement('h1', { className: "Appchild"}, 'Does this work now?'))
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
