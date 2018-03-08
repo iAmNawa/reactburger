acb8400652f9074622c5b23c9658fb494c93f223
Paul Borawski <paulborawski14@gmail.com>
2018-03-07 16:04:17 -0800

    paulyone and pauly2 are two different ways to update state

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'PaulyOne')}>Switch name</button>
+        <button onClick={() => this.switchNameHandler('PaulyOne')}>Switch name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
