630cc338d6ab7ae2e903492990a8ccedffa16582
Paul Borawski <paulborawski14@gmail.com>
2018-03-17 09:35:32 -0700

    full post becomes clicked post - INFINITE REQUEST BUG

--- practiceHttpAjax/http--01-starting-setup/src/components/FullPost/FullPost.js
+++ practiceHttpAjax/http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -4,11 +4,15 @@ import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+     loadedPost: null
+    }
+
     componentDidUpdate () {
       if (this.props.id) {
       axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
         .then(response => {
-          console.log(response);
+          this.setState({loadedPost: response.data})
         })
     }
   }
@@ -16,10 +20,13 @@ class FullPost extends Component {
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
           if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...!</p>
+          }
+          if (this.state.loadedPost) {
         post = (
             <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
+                <h1>{this.state.loadedPost.title}</h1>
+                <p>{this.state.loadedPost.content}</p>
                 <div className="Edit">
                     <button className="Delete">Delete</button>
                 </div>
