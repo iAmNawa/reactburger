6856338b39f0db82bf0e9ee2777a5dca68754534
Paul Borawski <paulborawski14@gmail.com>
2018-03-18 08:55:45 -0700

    using Link to render compnents rather than a full-page reload

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../FullPost/FullPost';
@@ -15,15 +15,17 @@ class Blog extends Component {
               <header>
                <nav>
                 <ul>
-                 <li><a href="/">Home</a></li>
-                 <li><a href="/new-post">New Post</a></li>
+                 <li><Link to="/">Home</Link></li>
+                 <li><Link to={{
+                     pathname: '/new-post'
+                   }}>New Post</Link></li>
                 </ul>
                </nav>
               </header>
              {/*}<Route path="/" exact render={() => <h1>Home</h1>} />
            <Route path="/" render={() => <h1>Home2</h1>} />*/}
-           <Route path="/" exact component={Posts} />
-           <Route path="/new-post" component={NewPost} />
+              <Route path="/" exact component={Posts} />
+              <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
