f0c898ebdf78b698b0f3ae57db2b9f3a5c4c0fec
Paul Borawski <paulborawski14@gmail.com>
2018-03-18 08:39:11 -0700

    use exact boolean to determine complete path

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from 'axios';
+//import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../FullPost/FullPost';
@@ -20,7 +20,7 @@ class Blog extends Component {
                 </ul>
                </nav>
               </header>
-                <Posts />
+             <Route path="/" exact render={() => <h1>Home</h1>} />
             </div>
         );
     }
