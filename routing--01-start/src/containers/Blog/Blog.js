f966add6a42142f9ddbde82caa0515441a02826e
Paul Borawski <paulborawski14@gmail.com>
2018-03-18 10:27:44 -0700

    NavLink will add an active class to the active route

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../FullPost/FullPost';
@@ -15,10 +15,12 @@ class Blog extends Component {
               <header>
                <nav>
                 <ul>
-                 <li><Link to="/">Home</Link></li>
-                 <li><Link to={{
-                     pathname: '/new-post'
-                   }}>New Post</Link></li>
+                 <li><NavLink to="/">Home</NavLink></li>
+                 <li><NavLink to={{
+                     pathname: '/new-post',
+                     hash: '#submit',
+                     search: '?quick-submit=true'
+                   }}>New Post</NavLink></li>
                 </ul>
                </nav>
               </header>
