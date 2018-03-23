aef7b613cff2ee8db7060919de2eb3a53b3ffb5f
Paul Borawski <paulborawski14@gmail.com>
2018-03-22 18:13:49 -0700

    nested routes with react router

--- routing--01-start/src/containers/Posts/Posts.js
+++ routing--01-start/src/containers/Posts/Posts.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 //import { Link } from 'react-router-dom';
 
 import Post from '../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
   state = {
@@ -33,8 +35,8 @@ class Posts extends Component {
 
   postSelectedHandler = (id) => {
       // EITHER OF THESE LINES WILL WORK
-      //this.props.history.push({pathname: '/' + id});
-      this.props.history.push('/' + id);
+      //this.props.history.push({pathname: '/posts/' + id});
+      this.props.history.push('/posts/' + id);
   }
 
   render() {
@@ -53,9 +55,12 @@ class Posts extends Component {
         });
     }
     return (
+      <div>
       <section className="Posts">
        {posts}
       </section>
+        <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+      </div>
     )
   }
 }
