acb287bf0fcdbc09f06606e076b0342a8ef366e4
Paul Borawski <paulborawski14@gmail.com>
2018-03-27 21:58:37 -0700

    withClass now wraps App and takes two arguments of the component you want to wrap and then the css className you wish to use...withClass is just a function that returns another function so it's not necessarily a higher order component but it serves the purpose of one

--- cmp-deep-dive--01-after-cmp-split/src/containers/App.js
+++ cmp-deep-dive--01-after-cmp-split/src/containers/App.js
@@ -3,6 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -85,7 +87,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
@@ -93,10 +95,10 @@ class App extends PureComponent {
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
         {persons}
-      </div>
+      </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
