9014916d685e4d304cbfb58a7b52eb91a18763ba
Paul Borawski <paulborawski14@gmail.com>
2018-03-29 09:14:24 -0700

    loading spinner will show for a second until firebase responds

--- http-burger--01-post-data/src/containers/BurgerBuilder/BurgerBuilder.js
+++ http-burger--01-post-data/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,13 +101,12 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fastest'
         }
          axios.post( '/orders.json', order )
-             .then( response => console.log(response))
-             .catch(error => console.log(error));
-        //         this.setState({ loading: false, purchasing: false });
-        //     } )
-        //     .catch( error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     } );
+              .then( response => {
+                this.setState({ loading: false, purchasing: false });
+              } )
+              .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+              } );
     }
 
     render () {
