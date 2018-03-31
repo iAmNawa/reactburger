9d0c7c75d45f2d29e968ac62d48a2c99f4aae978
Paul Borawski <paulborawski14@gmail.com>
2018-03-31 15:48:20 -0700

    adding checkout summary and checkout summary css- next is button handlers

---
+++ http-burger--01-post-data/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+      <div className={classes.CheckoutSummary}>
+        <div>We hope it tastes amazing!</div>
+        <div style={{width: '100%', height: '100%', margin: 'auto'}}>
+          <Burger ingredients={props.ingredients} />
+        </div>
+        <Button
+          btnType="Danger"
+          clicked>CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked>CONTINUE</Button>
+      </div>
+    );
+}
+
+export default checkoutSummary;
