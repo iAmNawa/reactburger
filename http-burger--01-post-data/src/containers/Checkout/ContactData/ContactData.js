251a464fa4ad786da60030caf3b1e315a3903e2d
Paul Borawski <paulborawski14@gmail.com>
2018-04-01 08:44:59 -0700

    new line for every input element

--- http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
+++ http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
@@ -17,10 +17,10 @@ class ContactData extends Component {
       <div className={classes.ContactData}>
        <h4>Enter your Contact Data</h4>
        <form>
-        <input type="text" name="name" placeholder="Your Name" />
-        <input type="email" name="name" placeholder="Your email" />
-        <input type="text" name="street" placeholder="Street" />
-        <input type="text" name="postal" placeholder="Postal Code" />
+        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+        <input className={classes.Input} type="email" name="name" placeholder="Your email" />
+        <input className={classes.Input} type="text" name="street" placeholder="Street" />
+        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
         <Button btnType="Success">ORDER</Button>
        </form>
       </div>
