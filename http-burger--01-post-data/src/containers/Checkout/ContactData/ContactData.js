70aaf24f05a5d5f72f460565dfb7094938aadbef
Paul Borawski <paulborawski14@gmail.com>
2018-04-02 11:28:28 -0700

    value should display true in console.log when typing

--- http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
+++ http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Your name'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             street: {
               elementType: 'input',
@@ -23,7 +27,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Street'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             zipCode: {
               elementType: 'input',
@@ -31,7 +39,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Zip code'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             country: {
               elementType: 'input',
@@ -39,7 +51,11 @@ class ContactData extends Component {
                 type: 'text',
                 placeholder: 'Country'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             email: {
               elementType: 'input',
@@ -47,7 +63,11 @@ class ContactData extends Component {
                 type: 'email',
                 placeholder: 'Email'
               },
-              value: ''
+              value: '',
+              validation: {
+                required: true
+              },
+              valid: false
             },
             deliveryMethod: {
               elementType: 'select',
@@ -84,6 +104,16 @@ class ContactData extends Component {
             } );
     }
 
+    checkValidity(value, rules) {
+      let isValid = false;
+
+       if (rules.require) {
+         isValid = value.trim() !== '';
+       }
+
+       return isValid;
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
       const updatedOrderForm = {
         ...this.state.orderForm
@@ -92,7 +122,9 @@ class ContactData extends Component {
         ...updatedOrderForm[inputIdentifier]
       };
       updatedFormElement.value = event.target.value;
+      updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
       updatedOrderForm[inputIdentifier] = updatedFormElement;
+      console.log(updatedFormElement);
       this.setState({orderForm: updatedOrderForm});
     }
 
