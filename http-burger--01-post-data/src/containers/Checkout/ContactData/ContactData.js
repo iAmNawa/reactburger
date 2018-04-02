94aaec9538cae0746ed51f053b13add135448ab7
Paul Borawski <paulborawski14@gmail.com>
2018-04-02 13:12:40 -0700

    console.log(formIsValid) returns true if ALL input fields are valid

--- http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
+++ http-burger--01-post-data/src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,11 @@ class ContactData extends Component {
                           {value: 'cheapest', displayValue: 'Cheapest'}
                 ]
               },
-              value: ''
+              value: '',
+              valid: true
             },
       },
+      formIsValid: false,
       loading: false
     }
 
@@ -140,8 +142,13 @@ class ContactData extends Component {
       updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
       updatedFormElement.touched = true;
       updatedOrderForm[inputIdentifier] = updatedFormElement;
-      console.log(updatedFormElement);
-      this.setState({orderForm: updatedOrderForm});
+
+      let formIsValid = true;
+      for (let inputIdentifier in updatedOrderForm) {
+        formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+      }
+      console.log(formIsValid)
+      this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
 
   render () {
@@ -165,7 +172,7 @@ class ContactData extends Component {
            touched={formElement.config.touched}
            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
        ))}
-       <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+       <Button btnType="Success" disabled={!this.state.formIsValid} clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
     if (this.state.loading) {
