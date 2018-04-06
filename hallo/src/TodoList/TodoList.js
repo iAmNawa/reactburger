e605fb3e7c866a4dd650d6477100b136ad191b60
Paul Borawski <paulborawski14@gmail.com>
2018-04-06 12:24:35 -0700

    CSS Transitions

--- hallo/src/TodoList/TodoList.js
+++ hallo/src/TodoList/TodoList.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react';
-import { CSSTransitionGroup } from 'react-transition-group'
+import { CSSTransitionGroup } from 'react-transition-group';
 import './TodoList.css';
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {items: ['hello', 'world', 'click', 'me']};
-    this.handleAdd = this.handleAdd.bind(this);
   }
-
-  handleAdd() {
-    const newItems = this.state.items.concat([
-      prompt('Enter some text')
-    ]);
-    this.setState({items: newItems});
-  }
-
-  handleRemove(i) {
-    let newItems = this.state.items.slice();
-    newItems.splice(i, 1);
-    this.setState({items: newItems});
-  }
-
   render() {
-    const items = this.state.items.map((item, i) => (
-      <div key={item} onClick={() => this.handleRemove(i)}>
-        {item}
-      </div>
-    ));
-
     return (
         <CSSTransitionGroup
       transitionName="example"
