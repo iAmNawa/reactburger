9770fbb82ed6218d6f37889d71859079264a40db
Paul Borawski <paulborawski14@gmail.com>
2018-04-08 17:39:44 -0700

    the users is reducer-users will now be users throughout the application

---
+++ React-Redux-Boilerplate/dev/js/reducers/reducer-users.js
@@ -0,0 +1,25 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: 'Paul',
+      last: 'Borawski',
+      age: 30,
+      description: 'Paul is a software developer'
+    },
+    {
+      id: 2,
+      first: 'Pauly',
+      last: 'Borawski',
+      age: 30,
+      description: 'Paul is a software developer'
+    },
+    {
+      id: 3,
+      first: 'Pauly B',
+      last: 'Borawski',
+      age: 30,
+      description: 'Paul is a software developer'
+    }
+  ]
+}
